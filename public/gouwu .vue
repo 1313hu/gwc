32945660379d6e4b6721dd17f6974e8b7217b6e3
19huhu <1684836558@qq.com>
2022-07-06 22:14:00 +0800

    完成了shopcar

--- public/gouwu .vue
+++ public/gouwu .vue
@@ -4,15 +4,14 @@
     <div style="margin-top:45px">
       <MyGoods v-for="item in list" :key="item.id" :obj="item"></MyGoods>
     </div>
-    <MyFooter @changeAll="allFn" :arr="list"></MyFooter>
-<MyCount></MyCount>
+    <MyFooter></MyFooter>
+
   </div>
 </template>
 <script>
 import MyFooter from "./components/MyFooter.vue"
 import MyGoods from "./components/MyGoods.vue"
 import MyHeader from "./components/MyHeader.vue"
-import MyCount from ".//components/MyCount.vue"
 export default {
   name: 'ShopcarApp',
 
@@ -27,7 +26,6 @@ export default {
     MyHeader,
     MyFooter,
     MyGoods,
-    MyCount
   },
  
  created() {
@@ -44,14 +42,9 @@ export default {
   },
 
   methods: {
-    allFn(val){
-      this.list.forEach(obj => obj.goods_state = val)
-    }
+    
   },
-  computed: {
-
-  }
-}
+};
 </script>
 
 <style lang="scss" scoped>
---
+++ src/components/01.compent.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h4>1. 动态dynamic组件使用</h4>
+    
+    <UseDynamic>
+    </UseDynamic>
+    <UserInfo></UserInfo>
+  </div>
+</template>
+
+<script>
+import UseDynamic from './components/01_UseDynamic.vue'
+import UserInfo from './components/UserInfo.vue'
+export default {
+  name: 'ShopcarApp',
+components: {
+  UseDynamic,
+  UserInfo
+},
+  data() {
+    return {
+      
+    };
+  },
+
+  mounted() {
+    
+  },
+
+  methods: {
+    
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
